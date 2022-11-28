6ebcf7040637bbba042c00e36496c30db38b6c72
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-11-29 01:15:24 +0300

    complete

---
+++ App.js
@@ -0,0 +1,154 @@
+import './App.css';
+import { ArtGrid } from './components/ArtGrid';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { About } from './components/About';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+let curAPIURLBase = 'https://dagmawibabi.com/tupm/addVisitorsCount/';
+let darkModeBGIndex = 0;
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [smallGrid, setSmallGrid] = useState(false);
+  const [magnifyOnHover, setMagnifyOnHover] = useState(true);
+  const [highQualityImages, setHighQualityImages] = useState(false);
+  const [showArtistDetail, setShowArtistDetail] = useState(true);
+  const [isExpanded, setExpanded] = useState(false);
+  const [visitorsCount, setVisitorsCount] = useState(0);
+
+  // let darkModeBGs = ["#fafafa", "#e4e5f1", "#d2d3db"]
+  function switchDarkMode() {
+    setDarkMode(!darkMode);
+    darkModeBGIndex++;
+    if(darkModeBGIndex > 3){
+      darkModeBGIndex = 0;
+    }
+
+  }
+  // Controls
+  // Small Grid
+  function changeGridSize(){
+    setSmallGrid(!smallGrid);
+  }
+  // Magnify on Hover
+  function changeMagnifyOnHover(){
+    setMagnifyOnHover(!magnifyOnHover);
+  }
+  // High Quality Images
+  function changeHighQualityImages(){
+    setHighQualityImages(!highQualityImages);
+  }
+  // Artist Detail
+  function changeShowArtistDetail(){
+    setShowArtistDetail(!showArtistDetail);
+  }
+  function changeExpand() {
+      setExpanded(!isExpanded);
+  }
+
+  // Visitors Count
+  // useEffect(()=>{
+  //   fetch(curAPIURLBase) 
+  //   .then((response) => {response.json(); })
+  //   .then((responseJSON) => {setVisitorsCount(responseJSON["visitorsCount"]); console.log(responseJSON)})
+  //   .catch((e) => console.log("error: " + e))
+  // }, []);
+  useEffect(() => {
+    axios.get(curAPIURLBase).then((data) => {
+      // console.log(data?.data["visitorsCount"]);
+      setVisitorsCount(data?.data["visitorsCount"]);
+    });
+  }, []);
+  return (
+    <div className="App" style={{backgroundColor: darkMode ? "#202020" : "white" } }> 
+      {/* H E A D E R */}
+      <Header darkMode={darkMode} switchDarkMode={switchDarkMode} />
+      {/* A B O U T */}
+      <About 
+        content={1} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+      />
+      {/* C O N T R O L S */}
+      <About 
+        content={6} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+      />
+      {/* G R I D */}
+      <ArtGrid 
+        content={1}
+        darkMode={darkMode} 
+        smallGrid={smallGrid} 
+        showArtistDetail={showArtistDetail} 
+        highQualityImages={highQualityImages} 
+        magnifyOnHover={magnifyOnHover} 
+        changeMagnifyOnHover={changeMagnifyOnHover}
+      />
+      {/* G A L L E R Y */}
+      <ArtGrid 
+        content={2}
+        darkMode={darkMode} 
+        smallGrid={smallGrid} 
+        showArtistDetail={showArtistDetail} 
+        highQualityImages={highQualityImages} 
+        magnifyOnHover={magnifyOnHover} 
+        changeMagnifyOnHover={changeMagnifyOnHover}
+      />
+      {/* D O W N L O A D */}
+      <About 
+        content={2} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+      />
+      {/* C O N T R I B U T E */}
+      <About 
+        content={3} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+      />
+      {/* C O N T A C T */}
+      <About 
+        content={4} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+      />
+      {/* T H A N K  Y O U  */}
+      <About 
+        content={5} 
+        darkMode={darkMode}
+        smallGrid={smallGrid} changeGridSize={changeGridSize} 
+        showArtistDetail={showArtistDetail} changeShowArtistDetail={changeShowArtistDetail}
+        highQualityImages={highQualityImages} changeHighQualityImages={changeHighQualityImages}        
+        magnifyOnHover={magnifyOnHover} changeMagnifyOnHover={changeMagnifyOnHover}
+        isExpanded={isExpanded} changeExpand={changeExpand}
+        visitorsCount={visitorsCount}
+      />
+      {/* F O O T E R */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/header.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './header.css'
+
+export const Header = (props) => {
+    let iryCount = 0;
+    const [titleHover, setTitleHover] = useState("")
+    function iry() {
+        if(props.darkMode === false){
+            iryCount += 1
+            if(iryCount === 7){
+                setTitleHover("I rain you med, all this art but you're the art I wanna look at forever!");
+            } else {
+                setTitleHover("")
+            }
+        }
+    }
+    return (
+        <div className='responsiveHeaderStyle'>
+            <div className='headerStyle' > 
+                <img className='darkMode' src={ props.darkMode ? require("../assets/icons/brightness.png") : require("../assets/icons/crescent-moon.png")} alt="lightmode" onClick={props.switchDarkMode} style={props.darkMode ? {width: "22px", height: "22px", cursor: "pointer"} : {width: "18px", height: "18px", cursor: "pointer"}} />
+                <div className='projectLogoAndName'>
+                    <img src={require("../assets/icons/unity.png")} alt="logo" className='projectLogo' />
+                    <span className='projectTitle' title={titleHover} onClick={iry}> The Unity Project Mural </span>
+                </div>
+                <img className='responsiveDarkMode' src={ props.darkMode ? require("../assets/icons/brightness.png") : require("../assets/icons/crescent-moon.png")} alt="lightmode" onClick={props.switchDarkMode} style={props.darkMode ? {width: "22px", height: "22px", cursor: "pointer"} : {width: "18px", height: "18px", cursor: "pointer"}} />
+                <div className='navigation'>
+                    <a href='#about' rel='noreferrer'> 
+                        <span> About </span>
+                    </a>
+                    <a href='#download' rel='noreferrer'> 
+                        <span> Download </span>
+                    </a>
+                    <a href='#contribute' rel='noreferrer'> 
+                        <span> Contribute </span>
+                    </a>
+                    <a href='#contact' rel='noreferrer'> 
+                        <span> Contact </span>
+                    </a>
+                    <img className='responsiveDarkMode2' src={ props.darkMode ? require("../assets/icons/brightness.png") : require("../assets/icons/crescent-moon.png")} alt="lightmode" onClick={props.switchDarkMode} style={props.darkMode ? {width: "22px", height: "22px", cursor: "pointer"} : {width: "22px", height: "22px", cursor: "pointer"}} />
+                </div>
+            </div>
+            <div className='responsiveNavigation'>
+                    <a href='#about' rel='noreferrer'> 
+                        <span> About </span>
+                    </a>
+                    <a href='#download' rel='noreferrer'> 
+                        <span> Download </span>
+                    </a>
+                    <a href='#contribute' rel='noreferrer'> 
+                        <span> Contribute </span>
+                    </a>
+                    <a href='#contact' rel='noreferrer'> 
+                        <span> Contact </span>
+                    </a>
+                </div>
+        </div>
+    );
+}
